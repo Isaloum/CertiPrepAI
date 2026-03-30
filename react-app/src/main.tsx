3f91603c8943b09927a4b847e7e2340e7f611c70
Ihab <ihabsaloum85@gmail.com>
2026-03-30 00:46:44 -0400

    Redirect old Amplify URL to awsprepai.isaloumapps.com

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Redirect old Amplify URL to custom domain
+if (window.location.hostname === 'main.d2pm3jfcsesli7.amplifyapp.com') {
+  window.location.replace('https://awsprepai.isaloumapps.com' + window.location.pathname + window.location.search)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
